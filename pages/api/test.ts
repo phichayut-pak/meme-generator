d1c5d50256a6c17572d09b7a7b4c6bc1c5628a69
AgilitiesZ <pakkung1245@gmail.com>
2022-07-30 11:35:23 +0700

    Update test.ts

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -7,7 +7,7 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
   await NextCors(req, res, {
       // Options
       methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-      origin: 'http://localhost:3000',
+      origin: 'https://google.com',
       optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   });
 
